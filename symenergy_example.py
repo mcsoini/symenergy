6befac4c0d0c461da78cce78c330188934b7720c
mcsoini <soini@posteo.de>
2018-10-10 17:37:09 +0200

    update

--- symenergy_example.py
+++ symenergy_example.py
@@ -24,41 +24,33 @@ import pyAndy.core.plotpage as pltpg
 
 reload(model)
 
-m = model.Model(curtailment=False)
+m = model.Model(curtailment=True, nthreads=7)
 
 self = m
 
 m.add_slot(name='day', load=4.5, vre=3)
 # m.add_slot(name='night', load=3, vre=1)
 m.add_slot(name='evening', load=5, vre=0.5)
->>>>>>> multiple_select_x
 
 #m.add_plant(name='b', vc0=1, vc1=0, slots=m.slots, capacity=1.5)
 m.add_plant(name='n', vc0=1, vc1=0, slots=m.slots, capacity=3,
-#            fcom=10,
-#            cap_ret=True
+            fcom=10,
+            cap_ret=True
             )
 m.add_plant(name='g', vc0=2, vc1=0, slots=m.slots)
 
-#m.add_storage(name='phs',
-#              eff=0.75,
-#              slots=m.slots,
-#              capacity=0.5,
-#              energy_capacity=1,
-#              slots_map={'day': 'chg',
-#                         'evening': 'dch'
-#                         })
+m.add_storage(name='phs',
+              eff=0.75,
+              slots=m.slots,
+              capacity=0.5,
+              energy_capacity=1,
+              slots_map={'day': 'chg',
+                         'evening': 'dch'
+                         })
 
 comp = m.comps['n']
 
-m.init_supply_constraints()
-m.init_constraint_combinations()
-
-m.remove_mutually_exclusive_combinations()
-
-m.delete_cached()
-
-m.solve_all(nthreads=7)
+m.generate_solve()
 
 m.filter_invalid_solutions()
 
@@ -68,26 +60,25 @@ reload(evaluator)
 
 m.comps['n'].vc1.value = 0
 m.comps['n'].vc0.value = 1.9 / 0.33 + 3.9
-#m.comps['n'].fcom.value = 20000 / 8760 * 2
-m.comps['n'].C.value = 3500
+m.comps['n'].fcom.value = 20000 / 8760 * 2
+m.comps['n'].C.value = 6666.6#3500
 
-#m.comps['g'].vc1.value = 0
-#m.comps['g'].vc0.value = 50 + 3
+m.comps['g'].vc1.value = 0
+m.comps['g'].vc0.value = 50 + 3
 
 m.slots['day'].vre.value = 1
 m.slots['day'].l.value = 4500
 #m.slots['evening'].vre.value = 0.1
 #m.slots['evening'].l.value = 3700
 
-#m.storages['phs'].eff.value = 0.75
+#m.storages['phs'].eff.value = 0.8
 #m.storages['phs'].C.value = 1000
 #m.storages['phs'].E.value = 20000
 
 m.vre_scale.value = 0.5
 
-select_x = m.vre_scale
-x_vals = {m.vre_scale: np.linspace(0, 9000, 41),
-          m.plants['n'].C: np.linspace(1000, 5000, 3)
+x_vals = {m.vre_scale: np.linspace(0, 9000, 31),
+          m.comps['n'].C: np.linspace(0, 10000 * 2/3, 2)
           }
 
 m.init_total_param_values()
@@ -110,12 +101,12 @@ ev.init_cost_optimum()
 ev.map_func_to_slot()
 
 ev.drop_non_optimal_combinations()
-
+# %%
 ev.line_plot(all_labels=False)
 
 ev.build_supply_table()
 
-ev.supply_plot(ind_axx='vre_scale', ind_plty='C_n')
+ev.supply_plot(ind_axx=['vre_scale'], ind_plty=['C_n'])
 
 
 
