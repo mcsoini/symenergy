58d627f3ae19524ba43cc0783bae3f96f6b5ee4c
mcsoini <soini@posteo.de>
2018-10-10 10:21:50 +0200

    new

--- symenergy_example.py
+++ symenergy_example.py
@@ -15,6 +15,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 from grimsel.auxiliary.aux_general import print_full
 
 import symenergy.core.model as model
@@ -23,7 +24,7 @@ import pyAndy.core.plotpage as pltpg
 
 reload(model)
 
-m = model.Model(curtailment=True)
+m = model.Model(curtailment=False)
 
 self = m
 
@@ -37,18 +38,18 @@ m.add_plant(name='n', vc0=1, vc1=0, slots=m.slots, capacity=3,
             cap_ret=True
             )
 m.add_plant(name='g', vc0=2, vc1=0, slots=m.slots)
-#
+
 m.add_storage(name='phs',
               eff=0.75,
               slots=m.slots,
               capacity=0.5,
-#              energy_capacity=1,
+              energy_capacity=1,
               slots_map={'day': 'chg',
                          'evening': 'dch'
                          })
 
 comp = m.comps['n']
-#
+
 m.init_supply_constraints()
 m.init_constraint_combinations()
 
@@ -56,7 +57,6 @@ m.remove_mutually_exclusive_combinations()
 
 m.delete_cached()
 
-# %
 m.solve_all(nthreads=7)
 
 m.filter_invalid_solutions()
@@ -65,15 +65,30 @@ m.filter_invalid_solutions()
 # %%
 reload(evaluator)
 
-#m.comps['n'].vc1.value = 0
-#m.comps['g'].vc1.value = 0
-##m.comps['phs'].C.value = 0.5
-#m.slots['day'].vre.value = 0.2
-m.slots['evening'].vre.value = 0.5
+m.comps['n'].vc1.value = 0
+m.comps['n'].vc0.value = 1.9 / 0.33 + 3.9
+m.comps['n'].fcom.value = 20000 / 8760 * 2
+m.comps['n'].C.value = 3500
+
+m.comps['g'].vc1.value = 0
+m.comps['g'].vc0.value = 50 + 3
+
+m.slots['day'].vre.value = 1
+m.slots['day'].l.value = 4500
+m.slots['evening'].vre.value = 0.1
+m.slots['evening'].l.value = 3700
+
+m.storages['phs'].eff.value = 0.75
+m.storages['phs'].C.value = 1000
+m.storages['phs'].E.value = 20000
+
+m.vre_scale.value = 0.5
 
 select_x = m.vre_scale
-select_x = m.comps['phs'].C
-x_vals = np.linspace(0, 4, 51)
+x_vals = np.linspace(0, 9000, 3)
+
+m.init_total_param_values()
+
 model = m
 ev = evaluator.Evaluator(model, select_x, x_vals)
 
@@ -83,34 +98,19 @@ list_dep_var = (list(map(str, list(self.model.variabs.keys()) + list(self.model.
 list_dep_var = ['tc'] + [v for v in list_dep_var if not 'lb_' in v]
 ev.get_evaluated_lambdas(list_dep_var)
 
-ev.expand_data_to_x_vals()
+ev.expand_to_x_vals()
 
 ev.df_exp = ev.model.combine_constraint_names(ev.df_exp)
 
-#ev.df_exp.loc[ev.df_exp.const_comb ==
-#      ('act_lb_n_pos_p_day=1, '
-#      'act_lb_n_pos_p_evening=1, '
-#      'act_lb_n_cap_C_day=0, '
-#      'act_lb_n_cap_C_evening=0, '
-#      'act_lb_g_pos_p_day=1, '
-#      'act_lb_g_pos_p_evening=1, '
-#      'act_lb_phs_pos_p_day=0, '
-#      'act_lb_phs_pos_p_evening=0, '
-#      'act_lb_phs_pos_e_None=0, '
-#      'act_lb_phs_cap_C_day=0, '
-#      'act_lb_phs_cap_C_evening=0, '
-#      'act_lb_curt_pos_p_day=0, '
-#      'act_lb_curt_pos_p_evening=1')]
-# %
 ev.enforce_constraints()
 
 ev.init_cost_optimum()
 
 ev.map_func_to_slot()
 
-ev.drop_non_optimal_combinations()
+#ev.drop_non_optimal_combinations()
 
-ev.line_plot(all_labels=True)
+ev.line_plot(all_labels=False)
 
 # %% BUILD SUPPLY CONSTRAINT
 
@@ -128,12 +128,11 @@ par_add = ['l', 'vre']
 pars = [getattr(slot, var) for var in par_add
        for slot in m.slots.values() if hasattr(slot, var)]
 
-
 df_bal_add = pd.DataFrame(df_bal[ev.select_x.name].drop_duplicates())
 for par in pars:
     df_bal_add[par.name] = par.value
 
-df_bal_add = df_bal_add.set_index('C_phs').stack().rename('lambd').reset_index()
+df_bal_add = df_bal_add.set_index('vre_scale').stack().rename('lambd').reset_index()
 df_bal_add = df_bal_add.rename(columns={'level_1': 'func'})
 df_bal_add['func_no_slot'] = df_bal_add.func.apply(lambda x: '_'.join(x.split('_')[:-1]))
 df_bal_add['slot'] = df_bal_add.func.apply(lambda x: x.split('_')[-1])
@@ -179,25 +178,3 @@ plt0.add_page_legend(lgdplotkey, hdl, lbl)
 
 
 
-# %%# adding symbols
-
-select_x_val = 1
-
-tc_subs_vals = m.param_values.copy()
-
-var_vals = ev.df_exp.loc[(ev.df_exp.const_comb == 'cost_optimum')
-                & (ev.df_exp.vre_scale == 0.5)]
-
-var_vals = var_vals.set_index('func')['lambd'].to_dict()
-
-list_vars = m.get_variabs_multips_slct(m.tc)
-
-dict_var_symb = {var: [symb for symb in list_vars if symb.name == var.replace('_lam_plot', '')] for var, val in var_vals.items() }
-dict_var_symb = {var: symb[0] for var, symb in dict_var_symb.items() if symb}
-
-dict_var_vals = {dict_var_symb[var]: val for var, val in var_vals.items() if var in dict_var_symb.keys()}
-
-tc_subs_vals.update(dict_var_vals)
-
-
-(m.tc.subs(tc_subs_vals), var_vals['tc_lam_plot'])
