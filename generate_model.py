cc8cfe72e9c7c44bd6ba14a716c869ded815d976
mcsoini <soini@posteo.de>
2018-11-08 15:33:41 +0100

    add get_model_lin function

--- generate_model.py
+++ generate_model.py
@@ -45,6 +45,36 @@ def get_model(solve=True, nthreads=7):
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+def get_model_lin(solve=True, nthreads=7):
+
+    m = model.Model(curtailment=True, nthreads=nthreads)
+
+    self = m
+
+    m.add_slot(name='day', load=4.5, vre=3)
+    m.add_slot(name='night', load=5, vre=0.5)
+
+    m.add_plant(name='n', vc0=1, vc1=1, slots=m.slots, capacity=3,
+                fcom=10,
+                cap_ret=True
+                )
+    m.add_plant(name='g', vc0=2, vc1=0, slots=m.slots)
+
+    m.add_storage(name='phs',
+                  eff=0.75,
+                  slots=m.slots,
+                  capacity=0.5,
+                  energy_capacity=1,
+                  slots_map={'day': 'chg',
+                             'night': 'dch'
+                             })
+    if solve:
+        m.generate_solve()
+
+    return m
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 def get_model_three(solve=True, nthreads=7):
 
     m_three = model.Model(curtailment=False, nthreads=nthreads)
