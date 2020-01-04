d16581e4f7584fa8d980d3fd46937fe6db7aaeb4
user <you@example.com>
2020-01-04 18:24:19 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
-import setuptools
+import fnmatch
+from setuptools import find_packages, setup
+from setuptools.command.build_py import build_py as build_py_orig
 
-with open("README.md", "r") as fh:
+with open("README_pypi.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="SymEnergy",
-    version="0.2",
+setup(
+    name="SymEnergy_test",
+    version="1.0.9",
     author="SymEnergy contributors listed in AUTHORS",
-    author_email="soini@posteo.de",
+    author_email="m.c.soini@posteo.de",
     description=("Lagrange multiplier based energy market toy "
                  "modeling framework"),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mcsoini/symenergy",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    install_requires=['pandas', 'orderedset', 'sympy', 'numpy', 'wrapt',
+                      'matplotlib', 'bokeh']
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD 2-Clause License",
-        "Operating System :: OS Independent",
-    ],
+        "Operating System :: OS Independent"],
 )
